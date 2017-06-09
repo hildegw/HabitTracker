8cad4e3fbceb2b9b21b5856f6ec1d1248442b404
hildegw <hildegw@snafu.de>
2017-06-09 17:28:12 -0500

    Contract and Helper done

--- app/src/main/java/com/example/android/habittracker/MainActivity.java
+++ app/src/main/java/com/example/android/habittracker/MainActivity.java
@@ -3,11 +3,26 @@ package com.example.android.habittracker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import data.DiveDbHelper;
+
 public class MainActivity extends AppCompatActivity {
+
+    DiveDbHelper mDiveDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //open helper instance to access DB
+        mDiveDbHelper = new DiveDbHelper(this);
+    }
+
+    private void insertDive() {
+
+    }
+
+    private void readDive() {
+
     }
 }
--- app/src/main/java/data/DiveDbHelper.java
+++ app/src/main/java/data/DiveDbHelper.java
@@ -11,29 +11,29 @@ public class DiveDbHelper extends SQLiteOpenHelper {
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "DiveLog.db";
 
-    //DB command strings for Create and Upgrad methods
-    private static final String SQL_CREATE =
-            "CREATE TABLE " +  DiveContract.DiveLog.TABLE_NAME + " (" +
-                    DiveContract.DiveLog._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
-                    DiveContract.DiveLog.DIVE_SITE + "TEXT NOT NULL," +
-                    DiveContract.DiveLog.DIVE_TYPE + "INTEGER NOT NULL" +
-                    DiveContract.DiveLog.DEPTH + "INTEGER" +
-                    DiveContract.DiveLog.DIVE_TIME + "INTEGER)";
-
-    private static final String SQL_UPGRADE =
-            "DROP TABLE IF EXISTS" + DiveContract.DiveLog.TABLE_NAME;
-
     //constructor
     public DiveDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-    //required methods
+    //required methods to override
+    @Override
     public void onCreate(SQLiteDatabase db) {
+        //SQL command string
+        String SQL_CREATE =
+                "CREATE TABLE " +  DiveContract.DiveLog.TABLE_NAME + " (" +
+                        DiveContract.DiveLog._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                        DiveContract.DiveLog.DIVE_SITE + "TEXT NOT NULL," +
+                        DiveContract.DiveLog.DIVE_TYPE + "INTEGER NOT NULL" +
+                        DiveContract.DiveLog.DEPTH + "INTEGER" +
+                        DiveContract.DiveLog.DIVE_TIME + "INTEGER)";
         db.execSQL(SQL_CREATE);
     }
-
+    @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //SQL command string
+        String SQL_UPGRADE =
+                "DROP TABLE IF EXISTS" + DiveContract.DiveLog.TABLE_NAME;
         db.execSQL(SQL_UPGRADE);
         onCreate(db);
     }
